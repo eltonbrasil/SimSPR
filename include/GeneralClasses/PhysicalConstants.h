801c76c9cf0b0e4549f1ddd0ec1d716a09b27156
eltonbrasil <eltonbrasil.kr@gmail.com>
2018-03-21 11:10:06 -0300

    Working on different files of SimSPR

--- include/GeneralClasses/PhysicalConstants.h
+++ include/GeneralClasses/PhysicalConstants.h
@@ -10,11 +10,9 @@ public:
 	constexpr static double c = 299792458;
 
 	// u_0 is the permeability of free space (H/m)
-
-	constexpr static double u_o = 4*V_PI*E-7
+	constexpr static double u_0 = 4*V_PI*E-7
 
 	// e_0 is the electric permitivity of free space (F/m)
-
 	constexpr static double e_0 = 8.854 * E-12
 
 } #endif // PHYSICALCONSTANTS_H
