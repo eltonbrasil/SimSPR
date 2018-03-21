97e23875e5ece3fc7dcdd1c0610c695465aed67e
eltonbrasil <eltonbrasil.kr@gmail.com>
2018-03-21 10:44:44 -0300

    GeneralClasses for SimSPR simulations

---
+++ include/GeneralClasses/PhysicalConstants.h
@@ -0,0 +1,20 @@
+#ifndef PHYSICALCONSTANTS_H
+#define PHYSICALCONSTANTS_H
+
+class PhysicalConstants
+{ 
+
+public:
+	
+	// c is the light`s velocity (m/s)
+	constexpr static double c = 299792458;
+
+	// u_0 is the permeability of free space (H/m)
+
+	constexpr static double u_o = 4*V_PI*E-7
+
+	// e_0 is the electric permitivity of free space (F/m)
+
+	constexpr static double e_0 = 8.854 * E-12
+
+} #endif // PHYSICALCONSTANTS_H
