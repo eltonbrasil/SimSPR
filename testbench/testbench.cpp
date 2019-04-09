8997e33a008031a8d227e84114b014dc06a910f2
eltonbrasil <eltonbrasil.kr@gmail.com>
2019-04-09 10:12:02 -0300

    :arrows_clockwise: Ploting SPR curve in real time using pipe interface

--- testbench/testbench.cpp
+++ testbench/testbench.cpp
@@ -2,8 +2,12 @@
 
 int main (void){
 
-    ofstream out_R_AIM;
-    out_R_AIM.open("reflectance.dat");
+    ofstream out;
+    out.open("reflectance.dat");
+
+    FILE *fp;
+
+    fp = popen( "gnuplot -persist", "w" );
 
     double r_metal;                 // Metal refractive index (real part)
     double i_metal;                 // Metal refractive index (imaginary part)
@@ -13,10 +17,8 @@ int main (void){
 	double wavelength;		        // Incident wavelength
     
     double theta_i = 0.1745;        // Start Incident Angle (in radians -> 10 degrees)
-    double theta_spr;               // Resonant Angle for WIM mode
-    double d_wim;                   // Metal Layer thickness value (nm) for WIM mode
-    
-    int N = 3;                      // Set number of Layers
+        
+    unsigned int N = 3;             // Set number of Layers
 
     double step_scale = 0.001;      // Set the step scale interval
     
@@ -98,7 +100,7 @@ int main (void){
             // Start timer
             timer.start();
             while (theta_i <= 1.5707){  // 90 in degrees                    
-                out_R_AIM << (theta_i * (180/M_PI)) << "\t\t" << kre_3.Reflectance(wavelength, n_prism, real[2], theta_i, real[1], imag[1], thickness[1]) << endl;
+                out << (theta_i * (180/M_PI)) << "\t\t" << kre_3.Reflectance(wavelength, n_prism, real[2], theta_i, real[1], imag[1], thickness[1]) << endl;
                 theta_i = theta_i + step_scale; 
             }
             // stop timer
@@ -115,7 +117,7 @@ int main (void){
             // Start timer
             timer.start();
             while (theta_i <= 1.5707){  // 90 in degrees                    
-                out_R_AIM << (theta_i * (180/M_PI)) << "\t\t" << kre_4.Reflectance(theta_i, wavelength, n_prism, real[1], imag[1], real[2], imag[2], real[3], thickness[1], thickness[2]) << endl;
+                out << (theta_i * (180/M_PI)) << "\t\t" << kre_4.Reflectance(theta_i, wavelength, n_prism, real[1], imag[1], real[2], imag[2], real[3], thickness[1], thickness[2]) << endl;
                 theta_i = theta_i + step_scale; 
             }
             // stop timer
@@ -139,7 +141,7 @@ int main (void){
     // Start timer
     timer.start();
         while (theta_i <= 1.5707){  // 90 in degrees                   
-            out_R_AIM << (theta_i * (180/M_PI)) << "\t\t" << OttoSpr.Reflectance(wavelength, n_prism, real[1], theta_i, real[2], imag[2], thickness[1]) << endl;
+            out << (theta_i * (180/M_PI)) << "\t\t" << OttoSpr.Reflectance(wavelength, n_prism, real[1], theta_i, real[2], imag[2], thickness[1]) << endl;
             theta_i = theta_i + step_scale; 
         }
     // stop timer
@@ -151,12 +153,30 @@ int main (void){
     
     cout << "The critical angle for this SPR configuration is: " << c_spr.TIR(real[1], n_prism) << " degrees" <<endl;
     cout << "The resonant angle for this SPR configuration is: " << t_spr.SPR_Angle(real[2], n_prism, real[1]) << " degrees" <<endl;
-
+   
     }
+
+    // ***
+    // An user C code can also activate gnuplot and pipe a gnuplot script into it. 
+    // ***
+
+    fprintf(fp, "set size ratio 0.5 \n");
+    fprintf(fp, "set title  \'SPR Curve\'\n" );
+    fprintf(fp, "set xlabel \'Incidence Angle (degrees)\'\n" );
+    fprintf(fp, "set ylabel \'Reflectance\'\n" );
+    fprintf(fp, "set grid \n");
+    fprintf(fp, "set xrange [0:90] \n");                       
+    fprintf(fp, "plot \'reflectance.dat\' using 1:2 with lines \n");
+    fflush (fp);
+    pclose(fp);
+
+    // ***
+    // 
+    // ***
 
     cout << endl;
 
-    out_R_AIM.close();
+    out.close();
         
     return 0;
 }
